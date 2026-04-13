9f559719dfeec2141dd2532589f6e4d9516430a2
Vladimir <vovasokolinskij@yandex.ru>
2026-04-13 16:56:23 +0800

    added headers file

---
+++ headers.py
@@ -0,0 +1,10 @@
+headers = {
+    "User-Agent": "Happ/3.13.0",
+    "X-Device-Os": "Android",
+    "X-Device-Locale": "ru",
+    "X-Device-Model": "ELP-NX1",
+    "X-Ver-Os": "15",
+    #"Accept-Encoding": "gzip",  # Requests обычно сам обрабатывает gzip.
+    "Connection": "close",
+    "X-Hwid": "74jf74nf8f4jr5je"
+}
