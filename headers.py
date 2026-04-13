e709c5d9c28eba04998de93194565497ec1b80a3
Vladimir <vovasokolinskij@yandex.ru>
2026-04-13 19:39:30 +0800

    Refactor headers.py to dynamically load configuration from config.yaml and generate it if missing

--- headers.py
+++ headers.py
@@ -1,9 +1,27 @@
+import yaml
+import os
+import secrets
+
+from devices import device_list
+
+if not os.path.exists("config.yaml"):
+    random_hwid = secrets.token_hex(8).upper()
+    random_device = secrets.choice(device_list)
+
+    with open("config.yaml", 'w') as f:
+        yaml.dump({"User-Agent": "v2raytun/android", "X-Device-Os": "Android", "X-Device-Locale": secrets.choice(["ru", "en"]), "X-Device-Model": str(random_device), "X-Ver-Os": str(9 + secrets.randbelow(7)), "Connection": "close", "X-Hwid": str(random_hwid)}, f)
+    print(f"Файл config.yaml не был найден и был создан автоматически. Пожалуйста, измените файл config.yaml.")
+    exit(1)
+else:
+    print(f"Файл config.yaml был найден.")
+    config = yaml.safe_load(open("config.yaml", 'r'))
+
 headers = {
-    "User-Agent": "Happ/3.13.0",
-    "X-Device-Os": "Android",
-    "X-Device-Locale": "ru",
-    "X-Device-Model": "ELP-NX1",
-    "X-Ver-Os": "15",
+    "User-Agent": config["User-Agent"],
+    "X-Device-Os": config["X-Device-Os"],
+    "X-Device-Locale": config["X-Device-Locale"],
+    "X-Device-Model": config["X-Device-Model"],
+    "X-Ver-Os": config["X-Ver-Os"],
     "Connection": "close",
-    "X-Hwid": "74jf74nf8f4jr5je"
+    "X-Hwid": config["X-Hwid"]
 }
